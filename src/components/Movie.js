636ca49a78cb7a4a018adf30c90d3d3f17ed7134
Benjamin Aragon <baragon23@gmail.com>
2020-05-29 13:28:30 -0700

    MovieInfo component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const Movie = ({ movieId }) => <div>Movie {movieId}</div>;
+import Actor from './elements/Actor';
+import Grid from './elements/Grid';
+import MovieInfo from './elements/MovieInfo';
+import MovieInfoBar from './elements/MovieInfoBar';
+import Navigation from './elements/Navigation';
+import Spinner from './elements/Spinner';
+
+import { useMovieFetch } from './hooks/useMovieFetch';
+
+const Movie = ({ movieId }) => {
+	const [movie, loading, error] = useMovieFetch(movieId);
+	console.log(movie);
+
+	if (error) return 'Someting went wrong...';
+	if (loading) return <Spinner />;
+
+	return (
+		<Fragment>
+			<Navigation movie={movie.original_title} />
+			<MovieInfo movie={movie} />
+			<MovieInfoBar />
+			<Grid>
+				<Actor />
+			</Grid>
+		</Fragment>
+	);
+};
 
 export default Movie;
---
+++ src/components/elements/Actor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NoImage from '../images/no_image.jpg';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
+import { StyledActor } from '../styles/StyledActor';
+
+const Actor = ({ actor }) => (
+	<StyledActor>
+		<img />
+	</StyledActor>
+);
+
+export default Actor;
